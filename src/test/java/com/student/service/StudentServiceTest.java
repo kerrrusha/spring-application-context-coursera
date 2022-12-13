ee26d2a096ebc462263b065b9be8c5da06da7cc1
Kirill Koval <brcyrill2@gmail.com>
2022-12-14 00:03:25 +0200

    init commit

---
+++ src/test/java/com/student/service/StudentServiceTest.java
@@ -0,0 +1,30 @@
+package com.student.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StudentServiceTest {
+	
+	private ClassPathXmlApplicationContext context;
+	private StudentService service;
+
+	@BeforeEach
+	void setUp() {
+		  
+	}
+	
+ 
+	@Test
+	void testGetOneStudent() {
+		  //TODO
+	}
+	
+	@Test
+	void tesGetAll() {
+		 
+	}
+}
